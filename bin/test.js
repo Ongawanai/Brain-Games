e7d57419a92d71bc6d04635e04c8fa7a0bc6509a
Ongawanai <ssaai@yandex.ru>
2022-11-29 14:26:52 +0500

    Put brain-input code into brain-input-code

--- bin/test.js
+++
@@ -1,6 +0,0 @@
-import readlineSync from 'readline-sync';
-let answer = '';
-answer = readlineSync.question('May I have your name? ');
-console.log(answer);
-answer = readlineSync.question('May I have your name? ');
-console.log(answer);
