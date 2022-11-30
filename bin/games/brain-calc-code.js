67b1e02831885538466c127c7a2fd166f72bd99d
Ongawanai <ssaai@yandex.ru>
2022-11-30 14:33:48 +0500

    Changed dangerous eval to Function on brain-calc

--- bin/games/brain-calc-code.js
+++ bin/games/brain-calc-code.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 const randomOperator = () => {
   const operators = ['+', '-', '*'];
-  let n = Math.floor(Math.random() * 3);
-  let operator = operators[n];
+  const n = Math.floor(Math.random() * 3);
+  const operator = operators[n];
   return operator;
 };
 
@@ -15,13 +15,15 @@ const calc = () => {
   let randomNumber1 = Math.floor(Math.random() * 21);
   let randomNumber2 = Math.floor(Math.random() * 21);
   let answer = 0;
+  let numberStr = '';
   let number = 0;
   let operator = randomOperator();
   for (let i = 0; i < 3; i += 1) {
     randomNumber1 = Math.floor(Math.random() * 21);
     randomNumber2 = Math.floor(Math.random() * 21);
     operator = randomOperator();
-    number = eval(randomNumber1 + operator + randomNumber2);
+    numberStr = `${randomNumber1} ${operator} ${randomNumber2}`;
+    number = Function('return ' + numberStr)();
     console.log(`Question: ${randomNumber1} ${operator} ${randomNumber2}`);
     answer = readlineSync.question('Your answer: ');
     if (Number(answer) === Number(number)) {
