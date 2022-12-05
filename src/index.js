eff1f4ae15ee23b4c034dbae9801c8658ddc1ff9
Ongawanai <ssaai@yandex.ru>
2022-12-05 17:05:01 +0500

    Extra small changes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const brainGame = (description, number, rightAnswer) => {
     console.log(`Question: ${question}`);
     answer = readlineSync.question('Your answer: ');
     if (String(answer) !== String(correctAnswer)) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
