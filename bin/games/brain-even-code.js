83670d47c16ee63000a07e1b23d8f180c0892100
Ongawanai <ssaai@yandex.ru>
2022-12-05 16:26:33 +0500

    Added "!" in the end of wrong answer line

--- bin/games/brain-even-code.js
+++ bin/games/brain-even-code.js
@@ -26,7 +26,7 @@ const isEven = () => {
     if (answer === rightAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
       i += 4;
     }
     if (i === 2) {
--- bin/games/brain-progression-code.js
+++ bin/games/brain-progression-code.js
@@ -22,20 +22,20 @@ const progression = () => {
   console.log(`Hello, ${userName}!`);
   console.log('What number is missing in the progression?');
   let randomIndex = 0;
-  let rightAnser = 0;
+  let rightAnswer = 0;
   let fullProgression = [];
   let answer = '';
   for (let i = 0; i < 3; i += 1) {
     fullProgression = randomProgression();
     randomIndex = Math.floor(Math.random() * 10);
-    rightAnser = fullProgression[randomIndex];
+    rightAnswer = fullProgression[randomIndex];
     fullProgression[randomIndex] = '..';
     console.log(`Question: ${fullProgression}`);
     answer = readlineSync.question('Your answer: ');
-    if (Number(answer) === Number(rightAnser)) {
+    if (Number(answer) === Number(rightAnswer)) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnser}'.\nLet's try again, ${userName}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
       i += 4;
     }
     if (i === 2) {
